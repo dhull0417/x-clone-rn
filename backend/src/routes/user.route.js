6e88aa11cb674c2ba40aafffcf49ed7df048ecce
Dallin Hull <dallin.hull@outlook.com>
2025-07-05 14:17:42 -0600

    User routes added

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { getUserProfile } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js"
+import { updateProfile } from "../controllers/user.controller.js";
+import { syncUser } from "../controllers/user.controller.js";
+import { getCurrentUser } from "../controllers/user.controller.js";
+import { followUser } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+
+// PUBLIC ROUTE
+// Return user profile
+router.get("/profile/:username", getUserProfile);
+
+//PROTECTED ROUTES
+// check authentication and then creat user in mongodb from clerk user data
+router.post("/sync", protectRoute, syncUser);
+
+// Get the current user (essentially, clicking on your own user profile rather than searching for a user profile)
+router.post("/me", protectRoute, getCurrentUser);
+
+// authenticate then update
+router.put("/profile", protectRoute, updateProfile);
+
+// To follow a user
+router.post("/follow/:targetUserId", protectRoute, followUser)
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,25 @@
 import express from "express";
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express";
+
+import userRoutes from "./routes/user.route.js";
+
 import {ENV} from "./config/env.js";
 import {connectDB} from "./config/db.js"
 
+const app = express();
 
-const app = express(); 
+// INITIALIZING MIDDLEWARE
+app.use(cors());
+// below is for access to the request.body
+app.use(express.json());
+// Bleow is for authentication
+app.use(clerkMiddleware());
 
-app.get("/", (req, res) => res.send("Hello from the database"))
+//INITIALIZING ROUTES
+app.get("/", (req, res) => res.send("Hello from the database"));
+
+app.use("/api/users", userRoutes);
 
 const startServer = async () => {
     try {
