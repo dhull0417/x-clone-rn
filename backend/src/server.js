15195b2ca260767e0afd5b6cb7cf5e71b47506d3
Dallin Hull <dallin.hull@outlook.com>
2025-07-09 17:08:51 -0600

    notifications-arcjet

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,11 @@ import {clerkMiddleware} from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import {ENV} from "./config/env.js";
 import {connectDB} from "./config/db.js"
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
@@ -17,6 +19,8 @@ app.use(cors());
 app.use(express.json());
 // Bleow is for authentication
 app.use(clerkMiddleware());
+// () means you are calling the function. We don't want to call the middleware because it's custom and we created it (unlike the middleware above)
+app.use(arcjetMiddleware)
 
 //INITIALIZING ROUTES
 app.get("/", (req, res) => res.send("Hello from the database"));
@@ -24,6 +28,7 @@ app.get("/", (req, res) => res.send("Hello from the database"));
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
