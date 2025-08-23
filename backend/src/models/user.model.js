9e920626c6c1c41e18588cafc59a3714e3b5fd1c
Dallin Hull <dallin.hull@outlook.com>
2025-08-22 19:56:04 -0600

    user FINALLY synced to db

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
 
         firstName: {
             type: String,
-            //required: true,
+            required: true,
             unique: true,
         },
 
